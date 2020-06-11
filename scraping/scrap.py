74454a9f13c347b6fd42d2e3043d106417c29561
penicillin0 <ayahitonakamura@gmail.com>
2020-06-11 22:51:38 +0900

    fix: json format

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -1,15 +1,24 @@
 import requests
 import bs4
 import re
+import json
 
 url = 'https://qiita.com/e869120/items/eb50fdaece12be418faa'
 
 res = requests.get(url)
 soup = bs4.BeautifulSoup(res.text, "html.parser")
-rep = soup.find_all('p')
-for r in rep:
-    result = re.match('.*<strong>\d+<\/strong>.*', str(r))
+p_DOM_soup_list = soup.find_all('p')
+
+problem_list = []
+
+for p_DOM_soup in p_DOM_soup_list:
+    result = re.match('.*<strong>\d+<\/strong>.*', str(p_DOM_soup))
     if result != None:
-        print(r.strong.text)
-        print(r.a.text)
-        print(r.a.get('href'))
+        problem = {
+            'problem_num': p_DOM_soup.strong.text,
+            'problem_name': p_DOM_soup.a.text,
+            'problem_url': p_DOM_soup.a.get('href')
+        }
+        problem_list.append(problem)
+        
+problem_list_json = json.dumps(problem_list, indent=2)
