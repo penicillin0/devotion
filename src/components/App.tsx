ce7561f02b7532698104a3066fa8b47cebc90452
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2020-06-19 22:53:44 +0900

    wip config firebase

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import firebase from "../firebase/clientApp";
 
-const App = ({ children }) => (
-  <main>
-    <Header />
-    {children}
-  </main>
-);
+const App = ({ children }) => {
+  useEffect(() => {
+    console.log(`[App.tsx:8] process.env: `, process.env);
+    // const practiceRef = firebase.database().ref("/practices");
+    // console.log(`[App.tsx:8] practiceRef: `, practiceRef);
+  }, []);
+
+  return (
+    <main>
+      <Header />
+      {children}
+    </main>
+  );
+};
 
 export default App;
