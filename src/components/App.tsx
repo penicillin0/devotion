09a4789d81359c04606a028b7156407c00964554
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2020-06-19 23:39:20 +0900

    Tweak

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,13 @@ import firebase from "../firebase/clientApp";
 
 const App = ({ children }) => {
   useEffect(() => {
-    console.log(`[App.tsx:8] process.env: `, process.env);
     const db = firebase.firestore();
     const practices = db
       .collection("practices")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`);
+          console.log(`${doc.id} => ${doc.data().title}`);
         });
       });
   }, []);
