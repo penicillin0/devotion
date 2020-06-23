e58b8cc660fad21a0f7c1c910b15acde3bdc7409
penicillin0 <ayahitonakamura@gmail.com>
2020-06-23 22:16:22 +0900

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,41 @@
 import * as React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+import { TiHome } from "react-icons/ti";
 
 const Header: React.FC<{ pathname?: string }> = ({ pathname }) => (
-  <header>
+  <HeaderBase>
+    <HomeLink href="/">
+      <HomeIcon></HomeIcon>
+    </HomeLink>
     <Link href="/">
       <a className={pathname === "/" ? "is-active" : ""}>Home</a>
     </Link>{" "}
     <Link href="/about">
       <a className={pathname === "/about" ? "is-active" : ""}>About</a>
     </Link>
-  </header>
+  </HeaderBase>
 );
+
+const HeaderBase = styled.header`
+  width: 1440px;
+  height: 100px;
+  position: fixed;
+  background-color: #ffffff;
+`;
+
+const HomeLink = styled.a`
+  display: flex;
+  align-items: center;
+`;
+
+const HomeIcon = styled(TiHome)`
+  width: 48px;
+  height: 48px;
+  padding-top: 28px;
+  padding-left: 48px;
+  color: #37add7;
+  cursor: pointer;
+`;
 
 export default Header;
