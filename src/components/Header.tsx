b1ac2602c251ca4e38ab9e769e9c2441145bcdc9
penicillin0 <ayahitonakamura@gmail.com>
2020-06-25 19:45:44 +0900

    ヘッダー影、横幅

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,10 +18,13 @@ const Header: React.FC<{ pathname?: string }> = ({ pathname }) => (
 );
 
 const HeaderBase = styled.header`
-  width: 1440px;
+  width: 100%;
   height: 100px;
   position: fixed;
   background-color: #ffffff;
+  z-index:1;
+  box-shadow: 0px 4px 4px rgba(0,0,0,0.25);;
+  
 `;
 
 const HomeLink = styled.a`
