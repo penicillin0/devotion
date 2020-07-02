e723190ec429442b8be59adf72aec637137eb5b3
penicillin0 <ayahitonakamura@gmail.com>
2020-07-03 04:29:45 +0900

    split problemItem

---
+++ src/components/PracticeItem.tsx
@@ -0,0 +1,72 @@
+import React, { useState, Props } from "react";
+import styled from "styled-components";
+import { FaRegCheckCircle, FaRegCircle } from "react-icons/fa";
+import { IoMdLink } from "react-icons/io";
+
+const LINK_HOVER_CLORO = `#0B8AFF`;
+const HeaderFontFamily = `"Poppins", "Noto Sans JP", sans-serif`;
+
+interface Practice {
+  title: string;
+  url: string;
+}
+
+const PracticeItem: React.FC<Practice> = ({ title, url }) => {
+  return (
+    <Base>
+      <CheckCircleIcon color="mediumseagreen"></CheckCircleIcon>
+      <PracticeItemTitle>{title}</PracticeItemTitle>
+      <PracticeLink href={url} target="_blank">
+        <LinkIcon></LinkIcon>
+      </PracticeLink>
+    </Base>
+  );
+};
+
+const Base = styled.div`
+  padding-bottom: 24px;
+  height: 72px;
+  display: flex;
+  align-items: center;
+`;
+
+const PracticeLink = styled.a`
+  display: flex;
+  align-items: center;
+`;
+
+const CheckCircleIcon = styled(FaRegCheckCircle)`
+  padding-right: 36px;
+  width: 40px;
+  height: 40px;
+`;
+
+const EmptyCircleIcon = styled(FaRegCircle)`
+  padding-right: 36px;
+  width: 40px;
+  height: 40px;
+`;
+
+const LinkIcon = styled(IoMdLink)`
+  width: 48px;
+  height: 48px;
+  padding-right: 48px;
+  color: #bdbdbd;
+  cursor: pointer;
+  &:hover {
+    color: ${LINK_HOVER_CLORO};
+  }
+`;
+
+const PracticeItemTitle = styled.div`
+  flex-basis: 500px;
+
+  font-size: 24px;
+  font-family: ${HeaderFontFamily};
+  cursor: pointer;
+  &:hover {
+    color: ${LINK_HOVER_CLORO};
+  }
+`;
+
+export default PracticeItem;
--- src/components/PracticeItemList.tsx
+++ src/components/PracticeItemList.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { IoMdLink } from "react-icons/io";
-import { FaRegCheckCircle, FaRegCircle } from "react-icons/fa";
+import PracticeItem from "./PracticeItem";
 import { practicesState } from "./App";
 import { useRecoilValue } from "recoil";
 
 type Props = {};
 
 const HeaderFontFamily = `"Poppins", "Noto Sans JP", sans-serif`;
-const LINK_HOVER_CLORO = `#0B8AFF`;
 
 const PracticeItemList: React.FC<Props> = (props) => {
   const practices = useRecoilValue(practicesState);
@@ -18,13 +16,7 @@ const PracticeItemList: React.FC<Props> = (props) => {
       <PracticeMenu>問題一覧</PracticeMenu>
       <PracticeItems>
         {practices.map((practice) => (
-          <PracticeItem>
-            <CheckCircleIcon color="mediumseagreen"></CheckCircleIcon>
-            <PracticeItemTitle>{practice.title}</PracticeItemTitle>
-            <PracticeLink href={practice.url} target="_blank">
-              <LinkIcon></LinkIcon>
-            </PracticeLink>
-          </PracticeItem>
+          <PracticeItem title={practice.title} url={practice.url}></PracticeItem>
         ))}
       </PracticeItems>
     </Base>
@@ -40,7 +32,7 @@ const Base = styled.div`
 `;
 
 const PracticeMenu = styled.div`
-  padding-top: 80px;  
+  padding-top: 80px;
   padding-left: 48px;
   font-size: 48px;
   font-family: ${HeaderFontFamily};
@@ -49,52 +41,6 @@ const PracticeMenu = styled.div`
 const PracticeItems = styled.div`
   padding-top: 36px;
   padding-left: 48px;
-`;
-
-const PracticeItem = styled.div`
-  padding-bottom: 24px;
-  height: 72px;
-  display: flex;
-  align-items: center;
-`;
-
-const PracticeLink = styled.a`
-  display: flex;
-  align-items: center;
-`;
-
-const CheckCircleIcon = styled(FaRegCheckCircle)`
-  padding-right: 36px;
-  width: 40px;
-  height: 40px;
-`;
-
-const EmptyCircleIcon = styled(FaRegCircle)`
-  padding-right: 38px;
-  width: 36px;
-  height: 36px;
-`;
-
-const LinkIcon = styled(IoMdLink)`
-  width: 48px;
-  height: 48px;
-  padding-right: 48px;
-  color: #bdbdbd;
-  cursor: pointer;
-  &:hover {
-    color: ${LINK_HOVER_CLORO};
-  }
-`;
-
-const PracticeItemTitle = styled.div`
-  flex-basis: 500px;
-
-  font-size: 24px;
-  font-family: ${HeaderFontFamily};
-  cursor: pointer;
-  &:hover {
-    color: ${LINK_HOVER_CLORO};
-  }
 `;
 
 export default PracticeItemList;
