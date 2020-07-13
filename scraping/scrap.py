ba59511fc5e1db207f3a8d6b167c7869d4cde83d
penicillin0 <ayahitonakamura@gmail.com>
2020-07-13 19:01:35 +0900

    データ追加用スクリプト

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -3,22 +3,47 @@ import bs4
 import re
 import json
 
-url = 'https://qiita.com/e869120/items/eb50fdaece12be418faa'
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+
+# credentials
+project_id = 'devotion-e7bd5'
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'projectId': project_id,
+})
+db = firestore.client()
+
+
+# パース
+url = 'https://qiita.com/e869120/items/eb50fdaece12be418faa'
 res = requests.get(url)
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 p_DOM_soup_list = soup.find_all('p')
 
-problem_list = []
 
 for p_DOM_soup in p_DOM_soup_list:
     result = re.match('.*<strong>\d+<\/strong>.*', str(p_DOM_soup))
     if result != None:
-        problem = {
-            'problem_num': p_DOM_soup.strong.text,
-            'problem_name': p_DOM_soup.a.text,
-            'problem_url': p_DOM_soup.a.get('href')
-        }
-        problem_list.append(problem)
-        
-problem_list_json = json.dumps(problem_list, indent=2)
+
+        # p DOM ごとの操作
+        problem_id_list = [
+            strong_dom.text for strong_dom in p_DOM_soup.find_all('strong')]
+        tile_list = [a_dom.text for a_dom in p_DOM_soup.find_all('a')]
+        url_list = [a_dom.get('href') for a_dom in p_DOM_soup.find_all('a')]
+
+        n = len(problem_id_list)
+        for i in range(n):
+            problem = {
+                'problem_id': int(problem_id_list[i]),
+                'title': tile_list[i],
+                'url': url_list[i],
+                'description': "",
+            }
+
+            # データ追加
+            doc_ref = db.collection(u'practices').document(
+                str(problem['problem_id']))
+            doc_ref.set(problem)
